26ebbe46a157532666e794f796fa7ef2f80f2b49
Arthur Monteiro <arthur.monteiro@grand.work>
2025-12-30 20:26:32 -0300

    chore: create palindromo project

---
+++ palindromo/bin/palindromo.dart
@@ -0,0 +1,5 @@
+import 'package:palindromo/palindromo.dart' as palindromo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${palindromo.calculate()}!');
+}
---
+++ palindromo/test/palindromo_test.dart
@@ -0,0 +1,8 @@
+import 'package:palindromo/palindromo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
