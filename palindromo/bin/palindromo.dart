d17489b0a840dd2980dc69815b020e6f1530faf7
Arthur Monteiro <arthur.monteiro@grand.work>
2025-12-30 20:50:42 -0300

    feat: palindromo

--- palindromo/bin/palindromo.dart
+++ palindromo/bin/palindromo.dart
@@ -1,5 +1,18 @@
-import 'package:palindromo/palindromo.dart' as palindromo;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${palindromo.calculate()}!');
+void main() {
+  print('------ Palíndromo ------');
+  print('> Informe sua frase:');
+
+  var input = stdin.readLineSync()?.replaceAll(' ', '').toLowerCase();
+  var reversed = input?.split('').reversed.join();
+  print('\n$input -> $reversed \n');
+
+  if (input == reversed) {
+    print('é um palindromo! \n');
+  } else {
+    print('não é um palindromo! \n');
+  }
+
+  print('------ END ------');
 }
--- palindromo/test/palindromo_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:palindromo/palindromo.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
