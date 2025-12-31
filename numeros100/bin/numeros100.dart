c3229f194f916fe5f2d5d3c3e6981c93579577d7
miguel-almeida2008 <miguelasouza02@gmail.com>
2025-12-31 12:38:57 -0300

    feat: exercício_resolvido

---
+++ numeros100/bin/numeros100.dart
@@ -0,0 +1,6 @@
+void main() {
+  for (int n = 0; n <= 100; n++) {
+    print(n);
+  }
+}
+//Teste
---
+++ numeros100/test/numeros100_test.dart
@@ -0,0 +1,8 @@
+import 'package:numeros100/numeros100.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
