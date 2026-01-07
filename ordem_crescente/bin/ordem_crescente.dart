d07854e77a521dcb4ad9f4b4f2a259e19273dab1
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-07 19:08:51 -0300

    feat: ordem_crescente

--- ordem_crescente/bin/ordem_crescente.dart
+++ ordem_crescente/bin/ordem_crescente.dart
@@ -1,5 +1,11 @@
-import 'package:ordem_crescente/ordem_crescente.dart' as ordem_crescente;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${ordem_crescente.calculate()}!');
+void main() {
+  print("Digite o primeiro número:");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Digite o segundo número:");
+  int b = int.parse(stdin.readLineSync()!);
+  List<int> numeros = [a,b];
+  numeros.sort();
+  print("${numeros[0]} ${numeros[1]}");
 }
--- ordem_crescente/test/ordem_crescente_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ordem_crescente/ordem_crescente.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
