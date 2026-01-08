71f4df2dfcf02295950ee97d9f7ca79135f44278
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-08 16:27:56 -0300

    chore: create angulo project

---
+++ angulo_triangulo/bin/angulo_triangulo.dart
@@ -0,0 +1,5 @@
+import 'package:angulo_triangulo/angulo_triangulo.dart' as angulo_triangulo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${angulo_triangulo.calculate()}!');
+}
---
+++ angulo_triangulo/test/angulo_triangulo_test.dart
@@ -0,0 +1,8 @@
+import 'package:angulo_triangulo/angulo_triangulo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
