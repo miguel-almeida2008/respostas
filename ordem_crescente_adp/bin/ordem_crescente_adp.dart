528199b937386921c901263db36b885b9cc9bba3
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-08 16:14:45 -0300

    chore: create ordem_crescente_adp project

---
+++ ordem_crescente_adp/bin/ordem_crescente_adp.dart
@@ -0,0 +1,5 @@
+import 'package:ordem_crescente_adp/ordem_crescente_adp.dart' as ordem_crescente_adp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ordem_crescente_adp.calculate()}!');
+}
---
+++ ordem_crescente_adp/test/ordem_crescente_adp_test.dart
@@ -0,0 +1,8 @@
+import 'package:ordem_crescente_adp/ordem_crescente_adp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
