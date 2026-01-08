329e8d29e41d2dc982b1be8e2952fd5ca9c0d43d
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-08 16:41:37 -0300

    feat: angulo_triangulo

--- angulo_triangulo/README.md
+++ angulo_triangulo/README.md
@@ -1,2 +1,1 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Escreva um programa em C que o usuário entre com dois ângulos internos de um triângulo e o programa calcule o 3o ângulo do triângulo.
--- angulo_triangulo/bin/angulo_triangulo.dart
+++ angulo_triangulo/bin/angulo_triangulo.dart
@@ -1,5 +1,10 @@
-import 'package:angulo_triangulo/angulo_triangulo.dart' as angulo_triangulo;
-
-void main(List<String> arguments) {
-  print('Hello world: ${angulo_triangulo.calculate()}!');
+import 'dart:io';
+void main() {
+  print("Digite o 1 angulo:");
+  int angulo1 = int.parse(stdin.readLineSync()!);
+  print("Digite o 2 angulo:");
+  int angulo2 = int.parse(stdin.readLineSync()!);
+  int angulo3 = 180 - (angulo1 - angulo2);
+  String anguloRestante = angulo3.toStringAsFixed(6);
+  print("O terceiro angulo é $anguloRestante");
 }
