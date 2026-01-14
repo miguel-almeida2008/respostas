400d3220cffb44071b4ccafc25e3f63cfd603d77
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-14 20:21:12 -0300

    Exercício/2 números em ordem crescente (#6)
    * feat: exercicio_numeros_em_sequencia
    
    * Exercício/números100
    
    * Exercício/numeros_em_sequencia
    
    * feat: unidades
    
    * feat:primeiros_multiplos
    
    * chore: create ordem_crescente project
    
    * feat: ordem_crescente
    
    * fix: remove primeiros_multiplos exercício
    
    ---------
    
    Co-authored-by: Arthur Monteiro <arthur.monteiro@grand.work>

---
+++ ordem_crescente/bin/ordem_crescente.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Digite o primeiro número:");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Digite o segundo número:");
+  int b = int.parse(stdin.readLineSync()!);
+  List<int> numeros = [a,b];
+  numeros.sort();
+  print("${numeros[0]} ${numeros[1]}");
+}
