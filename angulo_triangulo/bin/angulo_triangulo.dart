1aed323d1ad57825b5ac95949cd46df856a0d255
miguel-almeida2008 <miguelasouza02@gmail.com>
2026-01-14 20:31:17 -0300

    fix: triangle angle

--- angulo_triangulo/bin/angulo_triangulo.dart
+++ angulo_triangulo/bin/angulo_triangulo.dart
@@ -4,7 +4,7 @@ void main() {
   int angulo1 = int.parse(stdin.readLineSync()!);
   print("Digite o 2 angulo:");
   int angulo2 = int.parse(stdin.readLineSync()!);
-  int angulo3 = 180 - (angulo1 - angulo2);
+  int angulo3 = (180 - (angulo1 + angulo2));
   String anguloRestante = angulo3.toStringAsFixed(6);
   print("O terceiro angulo é $anguloRestante");
 }
